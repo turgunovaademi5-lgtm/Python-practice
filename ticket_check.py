43d5b7c8fe778bc71cd061859430a9df1d4002ca
turgunovaademi5-lgtm <turgunovaademi5@gmail.com>
2026-09-03 14:40:53 +0600

    Delete ticket_check.py

--- ticket_check.py
+++
@@ -1,5 +0,0 @@
-name = input("Введите ваше имя:")
-age = int(input("Введите ваш возрост:"))
-years = int(input("Через сколько лет вы хотите узнать свой возраст?:"))
-future_age = age + years
-print(f"{name}, через {years} лет вам будет {future_age} лет.")
