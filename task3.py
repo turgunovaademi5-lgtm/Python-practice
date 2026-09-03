61ca91c8cd1e570acafc69aaa486b820e7a71562
turgunovaademi5-lgtm <turgunovaademi5@gmail.com>
2026-09-03 14:43:12 +0600

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,11 @@
+grade = int(input('Введите вашу оценку: '))
+if grade == 5:
+	print('Отлично')
+elif grade == 4:
+	print('Хорошо') 
+elif grade == 3:
+	print('Неплохо')
+elif grade == 2:
+	print('Нужно еще потренироваться')
+else:
+	print('Такой оценки нет')
