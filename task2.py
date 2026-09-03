6be0e0dc0640ddf550f0428f1d11dbcb7c08c21e
turgunovaademi5-lgtm <turgunovaademi5@gmail.com>
2026-09-03 14:42:30 +0600

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,10 @@
+name = input('Как вас зовут? ')
+age = int(input('Сколько вам лет? '))
+height = float(input('Какой у вас рост? '))
+
+student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
